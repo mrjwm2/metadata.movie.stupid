43d6f822fde8605d6db37d36f1b7fb13ae33e4da
Ross Patterson <me@rpatterson.net>
2017-02-04 09:03:08 -0800

    Fix logging, use xbmc.log instead of stderr

--- service.py
+++ service.py
@@ -17,6 +17,36 @@ import httpdecho  # noqa
 ADDON = xbmcaddon.Addon()
 
 
+class StupidHTTPRequestHandler(httpdecho.EchoHTTPRequestHandler):
+    httpdecho.EchoHTTPRequestHandler.__doc__
+
+    log_level = xbmc.LOGDEBUG
+
+    def _log(self, format, args, level=None):
+        """
+        Common logging system call.
+        """
+        if level is None:
+            level = self.log_level
+        xbmc.log(
+            "metadata.movie.stupid: %s - - [%s] %s\n" % (
+                self.client_address[0], self.log_date_time_string(),
+                format % args),
+            level)
+
+    def log_message(self, format, *args):
+        """
+        Send log messages to the logging module instead of stderr.
+        """
+        self._log(format, args)
+
+    def log_error(self, format, *args):
+        """
+        Log at the correct level.
+        """
+        self.log_message(format, args, level=xbmc.LOGERROR)
+
+
 def main():
     """
     Start the echo server on login.
@@ -24,7 +54,7 @@ def main():
 
     httpd = BaseHTTPServer.HTTPServer(
         ('localhost', int(ADDON.getSetting("port"))),
-        httpdecho.EchoHTTPRequestHandler)
+        StupidHTTPRequestHandler)
     httpd_thread = threading.Thread(target=httpd.serve_forever)
     httpd_thread.start()
 
